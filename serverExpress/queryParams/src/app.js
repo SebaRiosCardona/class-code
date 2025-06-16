2e620d3d1039a1819f83ecb30676a81b37c0bbb5
jsebariosc <sebastian@rudol.ai>
2025-06-16 15:42:47 -0300

    clase 6 final

--- serverExpress/queryParams/src/app.js
+++ serverExpress/queryParams/src/app.js
@@ -10,7 +10,22 @@ let heroes = [
     id: 2,
     nombre:"superman",
     alias:"el loco maquina"
-  }
+  },
+  {
+    id: 3,
+    nombre:"superman",
+    alias:"el loco maquina"
+  },
+  {
+    id: 4,
+    nombre:"superman",
+    alias:"el loco maquina"
+  },
+  {
+    id: 5,
+    nombre:"superman",
+    alias:"el loco maquina"
+  },
 ]//un sniped es un arreglo de objeto con la misma info cada objeto
 
 const PORT = 3000
@@ -20,9 +35,22 @@ app.get("/", (req, res) => {//siempre esta funcion de callback va a recibir dos 
   res.send("home page")//Aca generamos la respues
 }) //ponemos get porque la peticion que queres hacer la emite un navegador y por defecto un navegador emite peticiones de tipo get
 app.get("/heroes", (req,res) => {
-  res.json(heroes)
-})
-app.get("/heroes/:id", (req,res) => { //ESTO EXPRESS LO VA A LEER COMO UN PARAMETRO QUE VA A LLEGAR DESDE LA URL, el cual lo podemos tomar de la siguiente forma
+
+  //en la req hay un objeto que se llama query que me permite pasar query params y si lo imprimo lo veo en la terminal
+  console.log(req.query)
+  let datos = heroes //esto lo hago para no modificar el arreglo original
+  let limit = req.query.limit //aca digo que limit es la propiedad limit de este objeto, y al pasarlo como queryParams lo uso como
+                              //limite para que me devuelva un parte del arreglo de objetos y no todo
+  if (limit && limit>0) { //Si llega un query que se llama limit y es mayor a cero que se imprima lo siguiente:
+    datos = datos.slice(0, limit) //slice es un metodo asociado a los elementos de tipo array para hacer un recorte, en este caso desde
+                                  //la posicion 0 hasta la posicion que indica limit (el cual indico por queryParams)
+                                  //Si no llega ningun queryParams que sea limit no se va a entrar a este ir y se va a devolver todos
+                                  //los datos del array, pero si entra se va a recortar el array desde 0 a limit
+  }
+
+  res.json(datos)
+}) 
+app.get("/heroes/:id", (req,res) => { //ESTO EXPRESS LO VA A LEER COMO UN PARAMETRO QUE VA A LLEGAR DESDE LA URL, el cual lo podemos tomar de la siguiente forma:
   let id = req.params.id//uso req porque voy a usar algo que va a llegar desde la peticion
   console.log(id)
   id = Number(id) //Aca convierto el tipo de dato del id (de string a numero). Si trato de aplicar esta conversion a un string que sea "seba", el resultado de hacer esto sobre un string que no sea un numero va a ser un tipo de dato denominado NotANumber (NAN)
---
+++ serverExpress/requireExports/src/classes/UserManager.js
@@ -0,0 +1,33 @@
+//como se trabaja con archivos y se carga en memoria el modulo de archivo tengo que hacer el fs
+const fs = require("fs")
+const crypto = require("crypto")
+const path = require("path")
+
+class UserManager { //no usan una clase como molda para crear instancias de la clase sino para agrupo los comportamientos que van a tener en esta clase
+  constructor(rutaArchivo) {//cuando demos de alta este userManager se va a tener que pasar la ruta del archivo
+    this.path = rutaArchivo//aca no definimos una propiedad sino un una ruta de archivo
+  }
+  //Ahora van los metodos
+  async getUsuario(){
+    if (fs.existsSync(this.path)) {//aca pregunto si el archivo existe, y si existe voy a pedir que haga algo
+      /*aca estoy diciendo que ejecute este metodo de leer, el cual devuelve una promesa. Que espere a que se resuelva la promesa,
+        y acese resultado devuelto, el cual va a ser un archivo en formato texto, lo mande como argumento del parse, entonces va a devolver el objeto*/
+      return JSON.parse(await fs.promises.readFile(this.path, {encoding:"utf-8"}))
+    } else {
+      return []; //si el archivo no existe no voy a leer nada entonces devuelvo un array vacio
+    }
+  }
+}
+
+//console.log(userManager.getUsuario) //esto va a devolver una promesa de arreglo vacio porque toda operacion async (es decir los return que tiene adentro) va a devolver una promesa
+//para resolver la promesa hago lo siguiente:
+const entorno = async () => {
+  let userManager = new UserManager("./data/ejercicio.json") // aca estoy enviando la ruta cuando llamo el constructor
+  try{
+    console.log(await userManager.getUsuario())
+  } catch (error){
+    console.log(error.message);
+  }
+}
+
+entorno() // esto me va a devolver el array de objetos que tengo en el archivo json: [ { usuario: 'seba' }, { usuario: 'anto' } ]
