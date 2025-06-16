3c27341982eb8918d7839ef9bb95b2be52a9865a
jsebariosc <sebastian@rudol.ai>
2025-06-16 16:19:00 -0300

    clase 6 fin

--- serverExpress/requireExports/src/app.js
+++ serverExpress/requireExports/src/app.js
@@ -2,7 +2,7 @@
 //falta saber trabajar con modulos que hayamos generado nosotros, por ejemplo del de UserManager.js. Para hacerlo necesito requerirlos
 //desde este archivo
 const express = require("express")
-const UserManager =  require("./classes/UserManager") // y con esto estamos cargando toda la logica que teniamos en este script a este archivo app.js
+const UserManager =  require("./classes/UserManager") // y con esto estamos cargando toda la logica que teniamos en este script a este archivo app.js asignado a la variable "UserManager"
 
 const PORT = 3000
 const app = express()
